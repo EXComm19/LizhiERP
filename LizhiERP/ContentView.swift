b70a7fd7e46659fa3eca5c466f3a80e51402c26e
Kehan Xu <1498994114@qq.com>
2026-01-22 15:49:27 +0800

    Initial Commit

---
+++ LizhiERP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LizhiERP
+//
+//  Created by EXC19 on 2026/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LizhiERP/LizhiERPApp.swift
@@ -0,0 +1,17 @@
+//
+//  LizhiERPApp.swift
+//  LizhiERP
+//
+//  Created by EXC19 on 2026/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LizhiERPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
